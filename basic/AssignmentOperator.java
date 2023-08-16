136704ba37f1a96f32241fb77940ea3962416d7e
Nandhini MK <113160031+Nandhini-2306@users.noreply.github.com>
2023-08-16 13:05:08 +0530

    Update AssignmentOperator.java

--- basic/AssignmentOperator.java
+++ basic/AssignmentOperator.java
@@ -3,7 +3,11 @@ package basic;
 public class AssignmentOperator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 27;
+		int b = 27;
+		if(a==b){
+		System.out.println("Same");
+		}
 
 	}
 
